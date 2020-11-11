1fc337234c660f1770834953d34d73cd7db03929
Munju Kam <70109541+kmunju@users.noreply.github.com>
2020-11-10 20:48:57 -0600

    Update question 2.R

--- question 2.R
+++ question 2.R
@@ -1,7 +1,6 @@
-# What is the toxicity of commonly reported chemicals?
-# install.packages("readxl")
 library(readxl)
 pdf <- read_excel("pdf.xlsx")
+pdf[,c(1)]<-NULL
 pdf[,c(1)]<-NULL
 
 pdf <- pdf %>% 
@@ -13,5 +12,10 @@ CarbonB <- pdf[grep('Carbon black', x=pdf$Chemical),]
 Talc <- pdf[grep('Talc', x=pdf$Chemical),]
 Retinol <- pdf[grep('Retinol', x=pdf$Chemical),]
 Cocamide <- pdf[grep('cocamide', x=pdf$Chemical),]
-
-# Question to some chemicals that either do not exist?? need more research
+Silica <- pdf[grep('Silica', x=pdf$Chemical),]
+# Lung scarring which leads to symptoms such as coughing, shortness of breath, weakness, and weight loss
+Mica <- pdf[grep('mica', x=pdf$Chemical),]
+#Too much intake-altered bone metabolism and altered metabolism of other fat-soluble vitamins
+Vitamin<- pdf[grep('vitamin', x=pdf$Chemical),]
+#(Combination of pure vitamin A and fatty acid palmitic acid)-Generally safe
+Retinylpalmitate<- pdf[grep('palmitate', x=pdf$Chemical),]
